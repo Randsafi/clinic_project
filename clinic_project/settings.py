4f19d979e5d3e9de6bd1a4385284e44bb8b685a5
Randsafi <rands068@gmail.com>
2025-05-31 01:41:15 +0300

    ac

--- clinic_project/settings.py
+++ clinic_project/settings.py
@@ -137,8 +137,8 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # المجلد الذي سيتم فيه جمع الملفات الستاتيكية
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- clinic_project/urls.py
+++ clinic_project/urls.py
@@ -22,9 +22,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('clinic.urls')),
-    path('api/', include('clinic.urls')),
-] +static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)\
- +static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+ #   path('api/', include('clinic.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#+static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)\
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
